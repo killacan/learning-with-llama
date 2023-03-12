8fbc1621cf6f35b138edb033cb459ccb99771770
Leisurellama <kameron000@yahoo.com>
2023-03-12 16:37:10 -0500

    fix build errors3

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -6,37 +6,15 @@ import { getServerSession } from "next-auth/next"
 import  NextAuth from "../pages/api/auth/[...nextauth]";
 
 
-const SignIn = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const SignIn = () => {
     // const { data: sessionData } = useSession();
 
     return (
         <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id)}>
-                        Sign in with {provider.name}
-                    </button>
-                </div>
-            ))}
+            <h2>Sign In</h2>
         </>
     );
 }
 
 export default SignIn;
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-    const session = await getServerSession(context.req, context.res, NextAuth);
-    
-    // If the user is already logged in, redirect.
-    // Note: Make sure not to redirect to the same page
-    // To avoid an infinite loop!
-    if (session) {
-      return { redirect: { destination: "/" } };
-    }
-  
-    const providers = await getProviders();
-    
-    return {
-      props: { providers: providers ?? [] },
-    }
-  }
