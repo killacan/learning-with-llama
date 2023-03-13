6c730aec911d879839bd0bb4c6223a7e1db29331
Leisurellama <kameron000@yahoo.com>
2023-03-12 20:32:07 -0500

    possible color change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='text-stone-100'>
+    <Html className='text-black bg-gradient-to-b from-indigo-800 to-[#7130A3]'>
       <Head />
       <title>Learning With Llama</title>
-      <body className="min-w-screen min-h-screen bg-gradient-to-b from-indigo-800 to-[#7130A3]">
+      <body className="min-w-screen min-h-screen bg-[#2A52BE]">
         <Main />
         <NextScript />
       </body>
